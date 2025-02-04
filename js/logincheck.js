f69d31d756494ca2637d755586e79de0c692d508
Steven Downing <133807656+FlamingGentry@users.noreply.github.com>
2025-02-04 12:50:35 -0500

    Update logincheck.js

--- js/logincheck.js
+++ js/logincheck.js
@@ -1,7 +1,7 @@
 async function checkLogin() {
     const loginUrl = "https://frontend-take-home-service.fetch.com";
     var username = document.getElementById("uname").value;
-    var email = document.getElementById("uname").value;
+    var email = document.getElementById("email").value;
     const response = await fetch(loginUrl, {
         body: JSON.stringify({
             name: username,
