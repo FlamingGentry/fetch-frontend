f6cbf1a9fec20da1dce816b47f6b4c96bfc028ad
FlamingGentry <nickdowning97@gmail.com>
2025-02-05 12:41:42 -0500

    Changing to request and testing send to endpoint

--- js/logincheck.js
+++ js/logincheck.js
@@ -2,11 +2,9 @@ async function checkLogin() {
     const loginUrl = "https://frontend-take-home-service.fetch.com/auth/login";
     var username = document.getElementById("uname").value;
     var email = document.getElementById("email").value;
-    const response = await fetch(loginUrl, {
-        body: JSON.stringify({
-            name: username,
-            email: email
-        }),
-        method: "POST",
-    });
+
+    const logRequest = new Request(loginUrl, {body: {name: username, email: email}, method: "POST"});
+    logRequest.credentials = "include";
+
+    const response = await fetch(logRequest);
 }
