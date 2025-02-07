00ee24f7015e10c71c8eec2647dcb17694e1a4f6
FlamingGentry <nickdowning97@gmail.com>
2025-02-07 12:31:14 -0500

    Connection made with endpoint on post, testing why js code not connecting

--- js/logincheck.js
+++ js/logincheck.js
@@ -2,22 +2,25 @@ async function checkLogin() {
     const loginUrl = "https://frontend-take-home-service.fetch.com/auth/login";
     var username = document.getElementById("uname").value;
     var email = document.getElementById("email").value;
-
-    const response = await fetch(loginUrl, {
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            name: username,
-            email: email
-        }),
-        method: "POST",
-    })
-    .then (response => response.json());
-    if (response.status == "ok") {
-        localStorage.setItem("token", response.token);
-        window.location.href = "searchpage.html";
-    } else {
-        document.getElementById("loginresult").innerHTML = "Invalid name or email";
-    }
+    document.getElementById("loginresult").innerHTML = JSON.stringify({
+        name: username,
+        email: email
+    });
+    // const response = await fetch(loginUrl, {
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //         name: username,
+    //         email: email
+    //     }),
+    //     method: "POST",
+    // })
+    // .then (response => response.json());
+    // if (response.status == "ok") {
+    //     localStorage.setItem("token", response.headers.getSetCookie("fetch-access-token"));
+    //     window.location.href = "searchpage.html";
+    // } else {
+    //     document.getElementById("loginresult").innerHTML = "Invalid name or email";
+    // }
 }
